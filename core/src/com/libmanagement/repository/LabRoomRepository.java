d07f0b2b2fea1067e00e3e132e746c435c436eaa
hugo775128583 <hugo775128583@gmail.com>
2016-01-01 19:31:48 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	core/src/com/libmanagement/repository/LabroomRepository.java

--- core/src/com/libmanagement/repository/LabRoomRepository.java
+++ core/src/com/libmanagement/repository/LabRoomRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 /**
- * Created by FlareMars on 2016/1/1
+ * Created by Hugo775128583 on 2016/1/1.
  */
 public interface LabRoomRepository extends JpaRepository<LabRoom,String> {
-
     @Query("select new LabRoom(id,department,roomNumber,manager) from LabRoom as l where l.type = ?1")
     List<LabRoom> getByType(Integer type);
+
+    @Query("select l from LabRoom as l where l.department = ?1 and l.roomNumber = ?2")
+    List<LabRoom> findByRoomNumber(String department,String roomNumber);
 }
