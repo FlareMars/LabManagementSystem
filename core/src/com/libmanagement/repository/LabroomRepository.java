c822e7e377f09befa2a75fc560f018faecc23f47
hugo775128583 <hugo775128583@gmail.com>
2016-01-01 18:44:33 +0800

    Perfect LabRoomRepository

--- core/src/com/libmanagement/repository/LabroomRepository.java
+++ core/src/com/libmanagement/repository/LabroomRepository.java
@@ -1,27 +1,16 @@
 package com.libmanagement.repository;
 
 import com.libmanagement.entity.LabRoom;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 /**
- * Created by FlareMars on 2015/11/28
+ * Created by FlareMars on 2016/1/1
  */
-public interface LabRoomRepository extends JpaRepository<LabRoom, String> {
+public interface LabRoomRepository extends JpaRepository<LabRoom,String> {
 
     @Query("select new LabRoom(id,department,roomNumber,manager) from LabRoom as l where l.type = ?1")
     List<LabRoom> getByType(Integer type);
-
-    @Query("select t from LabRoom as t")
-    Page<LabRoom> listLabRooms(Pageable page);
-
-    @Query("select t from LabRoom as t")
-    List<LabRoom> listLabRooms();
-
-    @Query("select t from LabRoom as t where t.department=?1 and t.roomNumber = ?1")
-    List<LabRoom> findByRoomNumber(String department, String roomNumber);
 }
